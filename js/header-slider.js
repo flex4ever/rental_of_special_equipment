067d37b67a505d37da21c13ee55f6c2657c9f8dd
scrxvv <dimag0767@gmail.com>
2023-03-03 17:29:43 +0400

    first-slider

---
+++ js/header-slider.js
@@ -0,0 +1,63 @@
+const BlockImg2 = document.querySelector('.block-header-background-img-1');
+const BlockImg3 = document.querySelector('.block-header-background-img-2');
+const BlockImg1 = document.querySelector('.rental');
+const btn1 = document.getElementById('left_first-slider');
+const btn2 = document.getElementById('right_first-slider');
+const scoreSlide = document.getElementById('score-slide');
+
+let page = 1;
+let score = 1;
+
+btn2.addEventListener('click', () => {
+    page++
+    if (page == 4){
+        page=1;
+        BlockImg1.classList.remove("not-active-background")
+        BlockImg3.classList.add("not-active-background")
+    }
+    if (page == 2) {
+        BlockImg2.classList.remove("not-active-background")
+        BlockImg1.classList.add("not-active-background")
+
+    }
+    if (page == 3) {
+        BlockImg3.classList.remove("not-active-background")
+        BlockImg2.classList.add("not-active-background")
+    }
+    scoreSlide.innerHTML = "0" + page + "/"
+    //if(page == 3) {
+    //BlockImg3.classList.remove("not-active-background")
+    //BlockImg1.classList.add("not-active-background")
+    //}
+    //mne uje othodit' nad :(
+})
+
+btn1.addEventListener('click', () => {
+    page++
+    if (page == 4){
+        page=1;
+        BlockImg1.classList.remove("not-active-background")
+        BlockImg3.classList.add("not-active-background")
+    }
+    if (page == 2) {
+        BlockImg3.classList.remove("not-active-background")
+        BlockImg1.classList.add("not-active-background")
+
+    }
+    if (page == 3) {
+        BlockImg2.classList.remove("not-active-background")
+        BlockImg3.classList.add("not-active-background")
+    }
+    scoreSlide.innerHTML = "0" + page + "/"
+    //if(page == 3) {
+    //BlockImg3.classList.remove("not-active-background")
+    //BlockImg1.classList.add("not-active-background")
+    //}
+    //mne uje othodit' nad :(
+})
+
+console.log(BlockImg1)
+console.log(BlockImg2)
+console.log(BlockImg3)
+console.log(btn1)
+console.log(btn2)
