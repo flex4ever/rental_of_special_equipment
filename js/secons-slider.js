9b03ff0ca9eec44f94165fe5f2c3ec752c210a3d
scrxvv <dimag0767@gmail.com>
2023-03-07 15:08:41 +0400

    block-slider-two

--- js/secons-slider.js
+++ js/secons-slider.js
@@ -9,55 +9,61 @@ const equipment_line4 = document.querySelector('.special-equipment_line3');
 let schet = 0;
 let str = 0;
 console.log(sliderLine);
-RIGHT_BTN.addEventListener('click', ()=>{
-    if(str == 4){
-        str=0;
-        schet=0;
-        sliderLine.style.right = schet + "px"; 
-        
+RIGHT_BTN.addEventListener('click', () => {
+    if (str == 4) {
+        str = 0;
+        schet = 0;
+        sliderLine.style.right = schet + "px";
+
     }
-    schet+=1600;
+    schet += 1600;
     sliderLine.style.right = schet + "px";
     str++;
-    if(schet > 5000){
+    if (schet > 5000) {
         schet = 0
         sliderLine.style.right = schet + "px";
         str = 0
     }
     console.log(schet, str)
 
-    if (schet == 4){
-        schet=2;
-        equipment_line1.classList.remove("active-line")
-        equipment_line2.classList.add("active-line")
+
+    schet++
+    if (schet == 5) {
+        schet=1;
+        equipment_line1.classList.remove("not_active-line")
+        equipment_line2.classList.add("not_active-line")
     }
-    if (schet == 3) {
-        equipment_line2.classList.remove("active-line")
-        equipment_line3.classList.add("active-line")
+    if (schet == 2) {
+        equipment_line2.classList.remove("not_active-line")
+        equipment_line3.classList.add("not_active-line")
 
     }
+    if (schet == 3) {
+        equipment_line3.classList.remove("not_active-line")
+        equipment_line4.classList.add("not_active-line")
+    }
     if (schet == 4) {
-        equipment_line3.classList.remove("active-line")
-        equipment_line4.classList.add("active-line")
+        equipment_line4.classList.remove("not_active-line")
+        equipment_line1.classList.add("not_active-line")
     }
 })
-LEFT_BTN.addEventListener('click', ()=>{
-    if (schet > 0){
-        schet-=1600;
+LEFT_BTN.addEventListener('click', () => {
+    if (schet > 0) {
+        schet -= 1600;
         sliderLine.style.right = schet + "px";
     }
-    if (schet < 0){
+    if (schet < 0) {
         schet = 1600;
-        schet = schet*3;
+        schet = schet * 3;
         sliderLine.style.right = schet + "px";
         str = 4;
-        
+
     }
-    if (str == 0){
+    if (str == 0) {
         schet = 1600;
-        schet = schet*3;
+        schet = schet * 3;
         sliderLine.style.right = schet + "px";
-        str =  4;
+        str = 4;
     }
     str--
 })
