3da4ddf491dd5e7e1d8fba2e22a8485850dad113
yan4ar <115583304+yan4ar@users.noreply.github.com>
2023-03-08 00:53:04 +0300

    done 2/2 not header and footer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.onload = function () {
     slideOne();
     slideTwo();
 }
@@ -9,60 +9,98 @@ let displayValTwo = document.getElementById("range2");
 let minGap = 0;
 let sliderTrack = document.querySelector(".menu__slider-track");
 let sliderMaxValue = document.getElementById("slider-1").max;
-function slideOne(){
-    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+function slideOne() {
+    if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
         sliderOne.value = parseInt(sliderTwo.value) - minGap;
     }
     displayValOne.textContent = sliderOne.value;
     fillColor();
 }
-function slideTwo(){
-    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+function slideTwo() {
+    if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
         sliderTwo.value = parseInt(sliderOne.value) + minGap;
     }
     displayValTwo.textContent = sliderTwo.value;
     fillColor();
 }
-function fillColor(){
+function fillColor() {
     percent1 = (sliderOne.value / sliderMaxValue) * 100;
     percent2 = (sliderTwo.value / sliderMaxValue) * 100;
     sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #FCB427 ${percent1}% , #FCB427 ${percent2}%, #dadae5 ${percent2}%)`;
-} 
-window.onload = function(){
+}
+window.onload = function () {
     slideOne();
     slideTwo();
 }
 
 
 const btn = document.getElementById("submit");
+let btnMask = document.querySelector(".menu__icon-btn-decor")
 let catalogText = document.querySelector(".menu__flex-text");
-btn.addEventListener("click",function(){
-    console.log(btn)
-    catalogText.classList.toggle("active")   
+btn.addEventListener("click", function () {
+    btnMask.classList.toggle("active-two");
+    btnMask.classList.toggle("active-three");
+    console.log(btn);
+    catalogText.classList.toggle("active");
     console.log(backSize);
 })
 
-
+let btnMaskTwo = document.querySelector(".menu__icon-btn-decor-two")
 const btnTwo = document.getElementById("btn-2")
 let catalogTextTwo = document.getElementById("flex-text-2")
-btnTwo.addEventListener("click",function(){
+btnTwo.addEventListener("click", function () {
+    btnMaskTwo.classList.toggle("active-two");
+    btnMaskTwo.classList.toggle("active-three");
     console.log(btnTwo)
     catalogTextTwo.classList.toggle("active")
-    console.log(backSize);
+
 })
+let btnMaskThree = document.querySelector(".menu__icon-btn-decor-three")
 const btnThree = document.getElementById("btn-3")
 let catalogTextThree = document.getElementById("flex-text-3")
-btnThree.addEventListener("click",function(){
-    catalogTextThree.classList.toggle("active") 
-   console.log(backSize);
+btnThree.addEventListener("click", function () {
+    catalogTextThree.classList.toggle("active")
+    btnMaskThree.classList.toggle("active-two");
+    btnMaskThree.classList.toggle("active-three");
 })
-
+let btnMaskFour = document.querySelector(".menu__icon-btn-decor-four")
 const btnFour = document.getElementById("btn-4")
 let catalogTextFour = document.getElementById("flex-text-4")
-btnFour.addEventListener("click",function(){
+btnFour.addEventListener("click", function () {
     catalogTextFour.classList.toggle("active")
-   console.log(backSize);
+    btnMaskFour.classList.toggle("active-two");
+    btnMaskFour.classList.toggle("active-three");
+
+
+
+
+
 })
+let btnModalOffTwo = document.getElementById("modalOff-2")
+let btnModalOff = document.getElementById("modalOff")
+let btnModalOn = document.getElementById("modal")
+let modal = document.querySelector(".contacts__modal")
+btnModalOn.onclick = function () {
+    modal.style.display = "block"
+}
+btnModalOff.onclick = function () {
+    modal.style.display = "none"
+}
+btnModalOffTwo.onclick = function () {
+    modal.style.display = "none"
+}
+
+let btnCatalog = document.getElementById("catalog");
+let catalogBlock = document.querySelector(".catalog__btn-none");
+btnCatalog.addEventListener("click", function () {
+    catalogBlock.classList.toggle("active-block");
+});
+let btnFilter = document.querySelector(".catalog__filter-block");
+let filterBlock = document.querySelector(".menu__filter");
+btnFilter.addEventListener("click", function () {
+    filterBlock.classList.toggle("active-right");
+});
+
 
 
 
