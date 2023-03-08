273a474d5b5c10124a9446dadc1fd35bb010ffc9
LeonidCher <4ls2005@mail.ru>
2023-03-08 13:57:09 +0400

    slider_line

--- js/secons-slider.js
+++ js/secons-slider.js
@@ -13,27 +13,7 @@ let line_score = 0;
 let line_page = 0;
 console.log(sliderLine);
 RIGHT_BTN.addEventListener('click', () => {
-    /*if (str == 4) {
-        str = 1;
-        console.log('11');
-        equipment_line4.classList.remove("active-line");
-        equipment_line1.classList.add("active-line");
-    } else if (str == 2) {
-        equipment_line1.classList.remove("active-line");
-        equipment_line2.classList.add("active-line");
-        console.log('112');
-    } else if (str == 3) {
-        equipment_line2.classList.remove("active-line");
-        equipment_line3.classList.add("active-line");
-        console.log('113');
-    } else if (str == 4) {
-        equipment_line3.classList.remove("active-line");
-        equipment_line4.classList.add("active-line");
-        console.log('114');
-    } else if (str > 4) {
-            str = 0;
-    }
-    */
+    
     if (str == 4) {
         str = 0;
         schet = 0;
@@ -42,6 +22,26 @@ RIGHT_BTN.addEventListener('click', () => {
     schet += 1600;
     sliderLine.style.right = schet + "px";
     str++;
+    if (str == 4) {
+        str = 1;
+        console.log('11');
+        equipment_line4.classList.remove("active-line");
+        equipment_line1.classList.add("active-line");
+    } else if (str == 1) {
+        equipment_line1.classList.remove("active-line");
+        equipment_line2.classList.add("active-line");
+        console.log('112');
+    } else if (str == 2) {
+        equipment_line2.classList.remove("active-line");
+        equipment_line3.classList.add("active-line");
+        console.log('113');
+    } else if (str == 3) {
+        equipment_line3.classList.remove("active-line");
+        equipment_line4.classList.add("active-line");
+        console.log('114');
+    } else if (str > 4) {
+            str = 0;
+    }
     if (schet > 5000) {
         schet = 0;
         sliderLine.style.right = schet + "px";
@@ -50,20 +50,20 @@ RIGHT_BTN.addEventListener('click', () => {
     console.log(schet, str);
 
     
-    if (line_page == 4) {
-        line_page = 0;
-        line_score = 0;
-        activeLine.style.right = line_score + "px";
-    }
-    line_score += 139;
-    activeLine.style.right = line_score + "px";
-    line_page++;
-    if (line_score > 450) {
-        line_score = 0
-        activeLine.style.right = line_score + "px";
-        line_page = 0
-    }
-     console.log(line_score, line_page);
+    // if (line_page == 4) {
+    //     line_page = 0;
+    //     line_score = 0;
+    //     activeLine.style.right = line_score + "px";
+    // }
+    // line_score += 139;
+    // activeLine.style.right = line_score + "px";
+    // line_page++;
+    // if (line_score > 450) {
+    //     line_score = 0
+    //     activeLine.style.right = line_score + "px";
+    //     line_page = 0
+    // }
+    //  console.log(line_score, line_page);
     
 })
 LEFT_BTN.addEventListener('click', () => {
@@ -83,26 +83,33 @@ LEFT_BTN.addEventListener('click', () => {
         schet = schet * 3;
         sliderLine.style.right = schet + "px";
         str = 4;
-    }
-    str--
-
-    /*if (str == 5) {
-        str = 1;
         console.log('11');
         equipment_line1.classList.remove("active-line");
         equipment_line4.classList.add("active-line");
-    } else if (str == 2) {
-        equipment_line4.classList.remove("active-line");
-        equipment_line3.classList.add("active-line");
+        console.log('11224323');
+    }
+
+    // if (str <= 0) {
+    //     str = 1;
+        
+    // } else 
+    if (str == 1) {
+        equipment_line2.classList.remove("active-line");
+        equipment_line1.classList.add("active-line");
         console.log('112');
-    } else if (str == 3) {
+    } else if (str == 2) {
         equipment_line3.classList.remove("active-line");
         equipment_line2.classList.add("active-line");
         console.log('113');
-    } else if (str == 4) {
-        equipment_line2.classList.remove("active-line");
-        equipment_line1.classList.add("active-line");
+    } else if (str == 3) {
+        equipment_line4.classList.remove("active-line");
+        equipment_line3.classList.add("active-line");
         console.log('114');
-    } 
-    */
+    } else if (str > 4) {
+            str = 0;
+    }
+    str--
+
+    
+    
 })
